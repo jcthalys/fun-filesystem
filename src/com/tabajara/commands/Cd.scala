adaf51f5b6e55942e88eb9b7dff935739f152d04
Thalys Gomes <jc.thalys@gmail.com>
2019-07-08 15:09:57 +0200

    Cd with relative paths

--- src/com/tabajara/commands/Cd.scala
+++ src/com/tabajara/commands/Cd.scala
@@ -9,8 +9,6 @@ class Cd(dirName: String) extends Command {
 
 
   def doFindEntry(root: Directory, path: String): DirEntry = {
-    val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
-
     @tailrec
     def findEntryHelper(currentDirectory: Directory, path: List[String]): DirEntry = {
       if (path.isEmpty || path.head.isEmpty) currentDirectory
@@ -22,7 +20,25 @@ class Cd(dirName: String) extends Command {
       }
     }
 
-    findEntryHelper(root, tokens)
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      if (path.isEmpty) result
+      else if(".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if("..".equals(path.head)) {
+        if (result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
+
+    // 1. tokens
+    val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+
+    // 1.5 eliminate/collapse relative tokens
+    val newTokens = collapseRelativeTokens(tokens, List())
+
+    // 2. navigate to the correct entry
+    if (newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
 
   override def apply(state: State): State = {
